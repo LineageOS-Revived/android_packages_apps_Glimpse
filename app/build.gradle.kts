917f8855b16c4a0b39f165f420f367f87e11b09c
SirRGB <sirrgb@proton.me>
2024-06-09 19:28:23 +0200

    downgrade requirements to A10
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         applicationId = "org.lineageos.glimpse"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
